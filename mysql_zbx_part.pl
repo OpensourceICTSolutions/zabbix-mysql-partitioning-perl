8e78d2619d59c70dca6a75108bbbd8b69d5b3208
nliefting <nathan@oicts.nl>
2022-02-20 15:14:44 +0100

    Make the script Zabbix 6 compatible

--- mysql_zbx_part.pl
+++ mysql_zbx_part.pl
@@ -182,5 +182,6 @@ sub date_next_part {
 sub delete_old_data {
 	$dbh->do("DELETE FROM sessions WHERE lastaccess < UNIX_TIMESTAMP(NOW() - INTERVAL 1 MONTH)");
 	$dbh->do("TRUNCATE housekeeper");
-	$dbh->do("DELETE FROM auditlog_details WHERE NOT EXISTS (SELECT NULL FROM auditlog WHERE auditlog.auditid = auditlog_details.auditid)");
+# Uncomment the following line for Zabbix 5.4 and earlier
+#	$dbh->do("DELETE FROM auditlog_details WHERE NOT EXISTS (SELECT NULL FROM auditlog WHERE auditlog.auditid = auditlog_details.auditid)");
 }
